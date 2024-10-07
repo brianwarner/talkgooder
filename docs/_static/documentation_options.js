c7b84b482efdf3f001e344d201d1944db375fa0f
Brian Warner <brian@bdwarner.com>
2024-10-07 00:57:29 -0400

    Point docs to github.io instead of custom domain (#5)
    Point docs to github.io instead of custom domain and update author of documentation.
    
    Signed-off-by: Brian Warner <brian@bdwarner.com>

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.post1.dev1+gdb789d0',
+    VERSION: '0.0.post1.dev1+gdebd171',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
