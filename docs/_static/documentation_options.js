2d50de47da29d1a8914cf744072ce581fac99562
Brian Warner <brian@bdwarner.com>
2024-11-23 22:26:37 -0500

    Chores: Update workflows, docs, and VSCode settings
    Signed-off-by: Brian Warner <brian@bdwarner.com>

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.post1.dev1+ge624e3d',
+    VERSION: '0.0.post1.dev1+gbbb2af3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
