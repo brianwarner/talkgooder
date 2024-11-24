e957bcfa58124ad8c0ad50fea3d9d9fc2250933e
Brian Warner <brian@bdwarner.com>
2024-11-24 03:22:37 +0000

    Rebuild the documentation
    Signed-off-by: Brian Warner <brian@bdwarner.com>

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.1.0.post1.dev1+g4071045.d20241124',
+    VERSION: '0.0.post1.dev1+gbbb2af3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
