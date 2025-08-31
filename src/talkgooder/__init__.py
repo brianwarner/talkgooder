4fbc35a788581af3cc2503a053b43c950447e11c
Brian Warner <brian@bdwarner.com>
2025-08-30 23:20:20 -0400

    Chore: Update copyright notice
    Signed-off-by: Brian Warner <brian@bdwarner.com>

--- src/talkgooder/__init__.py
+++ src/talkgooder/__init__.py
@@ -4,7 +4,7 @@ talkGooder
 
 `talkgooder` attempts to smooth out grammar, punctuation, and number-related corner cases when formatting text for human consumption.
 
-:copyright: (c) 2024 by Brian Warner
+:copyright: (c) Brian Warner
 :license: MIT, see LICENSE.md for more details.
 """  # noqa: E501
 
